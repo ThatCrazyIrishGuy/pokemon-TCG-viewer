23b8954be355e1f4e28cd9195f8a4e8772a294b6
Darren Britton <darrenbritton@hotmail.com>
2016-04-02 12:46:56 +0100

    added search functionality

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-/*
-
-TV SHOW DATA
-
-*/
-
 $.getJSON('dataArray.json', function(json) {
     console.log(json);
     subset = json.slice(0, 12);
-    subset.forEach(function(card) {
+    displayCards(subset)
+
+    $('#search').keypress(function(e) {
+        var searchString = $('#search').val();
+        if (e.which == 13) {
+            subset = json.filter(function(card) {
+                if (card.name.toLowerCase() == searchString) return card;
+            });
+            subset = subset.slice(0, 12);
+            displayCards(subset);
+        }
+    });
+});
+/*
+
+HOVERING THING
+
+*/
+var array = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+var x = function(a) {
+    $(".hover:nth-child(" + a + ")").hover(function() {
+        $(".bg").addClass(array[a]);
+    }, function() {
+        $(".bg").removeClass(array[a]);
+    });
+}
+for (var i = 0; i < 10; i++) {
+    x(i);
+}
+
+function displayCards(cards) {
+    $('.footer').empty();
+    cards.forEach(function(card) {
         var id = card.image.replace(/\W/g, '');
         $('.footer').append(`<div style="background-image: url('${card.image}')" class="box active" id="${id}"></div>`)
         $('<img/>').attr('src', 'card.image').load(function() {
@@ -38,20 +64,4 @@ $.getJSON('dataArray.json', function(json) {
                 }, 500);
         });
     });
-});
-/*
-
-HOVERING THING
-
-*/
-var array = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-var x = function(a) {
-    $(".hover:nth-child(" + a + ")").hover(function() {
-        $(".bg").addClass(array[a]);
-    }, function() {
-        $(".bg").removeClass(array[a]);
-    });
-}
-for (var i = 0; i < 10; i++) {
-    x(i);
 }
